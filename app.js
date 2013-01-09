fbfffc89f1256159716d1d217fd40dee8959cadb
nucleardreamer <nucleardreamer@gmail.com>
2013-01-09 13:52:44 -0800

    way different

---
+++ app.js
@@ -0,0 +1,45 @@
+var io = require('socket.io');
+var express = require('express');
+var app = require('express')(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server),
+	request = require('request'),
+	arduino = require('./'),
+    board, sensor, ping;
+ 
+board = new arduino.Board({debug: false});
+
+sensor = new arduino.Sensor({
+	board: board,
+	pin: 'A0'
+});
+
+var photosensor = {
+	data: 0
+}
+
+server.listen(8080);
+
+app.get('/', function (req, res) {
+
+  res.sendfile(__dirname + '/public/index.html');
+  
+});
+
+io.sockets.on('connection', function (socket) {
+
+	socket.emit('init', { init: 'true' });
+	
+	setInterval(function(){
+		socket.emit('sensor',{
+			photo: photosensor
+		});
+	},10)
+	
+});
+
+sensor.on('read', function(err, value) {
+	value = +value;
+	console.log(value);
+	photosensor.data = value;
+});
